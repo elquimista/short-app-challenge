1ccf168202939fc55933f26231ea85569ca056c4
Vin Kim <vinkim002@gmail.com>
2021-12-13 20:59:35 -0500

    Implement #short_code, #find_by_short_code

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -4,7 +4,32 @@ class ShortUrl < ApplicationRecord
 
   validate :validate_full_url
 
+  def self.find_by_short_code(short_code)
+    id = 0
+
+    short_code.chars.each do |char|
+      id *= CHARACTERS.size
+      id += if ('0'..'9').include?(char)
+        char.ord - '0'.ord
+      elsif ('a'..'z').include?(char)
+        char.ord - 'a'.ord + 10
+      else
+        char.ord - 'A'.ord + 36
+      end
+    end
+
+    find_by(id: id)
+  end
+
   def short_code
+    id = self.id
+    short_code = []
+    loop do
+      short_code << CHARACTERS[id % CHARACTERS.size]
+      id /= CHARACTERS.size
+      break unless id > 0
+    end
+    short_code.reverse.join('')
   end
 
   def update_title!
