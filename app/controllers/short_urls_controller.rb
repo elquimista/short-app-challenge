2542e76c86207fcdcd980f09a7ae6ac111f6fe8e
Vin Kim <vinkim002@gmail.com>
2021-12-13 23:06:11 -0500

    Redirect to full url on visiting short code

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -10,6 +10,12 @@ class ShortUrlsController < ApplicationController
   end
 
   def show
+    short_url = ShortUrl.find_by_short_code(params[:id])
+    if short_url.present?
+      redirect_to short_url.full_url
+    else
+      head :not_found
+    end
   end
 
 end
