237e41b7eaeeeacd2ea966a9f81a77d4deff5621
Vin Kim <vinkim002@gmail.com>
2021-12-16 10:29:15 -0500

    Add title to MostFrequentlyAccessedUrls

--- app/javascript/src/components/MostFrequentlyAccessedUrls.jsx
+++ app/javascript/src/components/MostFrequentlyAccessedUrls.jsx
@@ -1,5 +1,7 @@
-import { Table } from 'antd'
+import { Table, Typography } from 'antd'
 import React, { useEffect, useState } from 'react'
+
+const { Title } = Typography
 
 const MostFrequentlyAccessedUrls = () => {
   const [urls, setUrls] = useState([])
@@ -11,19 +13,22 @@ const MostFrequentlyAccessedUrls = () => {
   }, [])
 
   return (
-    <Table
-      columns={[{
-        title: 'URL',
-        dataIndex: 'full_url',
-        key: 'url',
-      }, {
-        title: 'Click Count',
-        dataIndex: 'click_count',
-        key: 'clickCount',
-      }]}
-      dataSource={urls}
-      rowKey="id"
-    />
+    <>
+      <Title level={2}>Top 100 most frequently accessed URLs</Title>
+      <Table
+        columns={[{
+          title: 'URL',
+          dataIndex: 'full_url',
+          key: 'url',
+        }, {
+          title: 'Click Count',
+          dataIndex: 'click_count',
+          key: 'clickCount',
+        }]}
+        dataSource={urls}
+        rowKey="id"
+      />
+    </>
   )
 }
 
